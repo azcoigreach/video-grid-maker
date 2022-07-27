d0bce8008000ae374b9a0c236a0326d45a2c9985
azcoigreach <azcoigreach@gmail.com>
2022-07-27 10:35:36 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Video Grid Maker
+Proceedurally create video test grids for LED and Projected video projects
---
+++ app/main.py
@@ -0,0 +1,6 @@
+'''
+Proceedurally create video test patters for LED and Projected video Projects
+
+Tech Stack: Python3 pillow fast-api
+'''
+import pillow
